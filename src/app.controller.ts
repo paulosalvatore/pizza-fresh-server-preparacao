31183c4d9b36b72964537e017714912410ec41a3
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 10:35:43 -0300

    feat(app): add status endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAppStatus(@Req() req: Request): string {
+    const fullUrl = req.protocol + '://' + req.get('host');
+    return this.appService.getAppStatus(fullUrl);
   }
 }
