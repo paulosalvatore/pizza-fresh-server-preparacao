17f4e43a7ba017875686e51729587c38a1195873
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 11:19:50 -0300

    docs(app): add docs decorators

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Headers, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Status da aplicação',
+  })
   getAppStatus(@Req() req: Request): string {
     const fullUrl = req.protocol + '://' + req.get('host'); // http://localhost:3333
     return this.appService.getAppStatus(fullUrl);
