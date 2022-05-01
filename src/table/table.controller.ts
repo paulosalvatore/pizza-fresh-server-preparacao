cf9be6cb98a97f76bb70c3ed0505e608676dc369
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 11:19:41 -0300

    docs(table): add docs decorators

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,17 +1,25 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/create-table.dto';
 import { TableService } from './table.service';
 
+@ApiTags('table')
 @Controller('table')
 export class TableController {
   constructor(private tableService: TableService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todas as mesas',
+  })
   findAll() {
     return this.tableService.findAll();
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar uma mesa',
+  })
   create(@Body() createTableDto: CreateTableDto) {
     return this.tableService.create(createTableDto);
   }
