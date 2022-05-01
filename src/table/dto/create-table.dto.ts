73665ca0d73f0f98f90e89d654d7ba5f1dd2a0c3
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 11:20:00 -0300

    docs(table): add docs decorators to `CreateTableDto`

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 
 export class CreateTableDto {
+  @ApiProperty({
+    description: 'Número da mesa',
+    example: 1,
+  })
   @IsNumber()
   number: number;
 }
