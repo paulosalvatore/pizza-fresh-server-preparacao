cbae83d7d8318be6012fee458fb78ff661f970d8
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-01 11:05:47 -0300

    feat(table): implement create endoint in local memory

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTableDto } from './dto/create-table.dto';
+import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
+  tables: Table[] = [];
+
   findAll() {
     return 'Buscar todas as mesas';
   }
 
-  create() {
-    return 'Adicionar uma mesa';
+  create(createTableDto: CreateTableDto) {
+    const table: Table = { id: 'random_id', ...createTableDto };
+
+    this.tables.push(table);
+
+    return table;
   }
 }
