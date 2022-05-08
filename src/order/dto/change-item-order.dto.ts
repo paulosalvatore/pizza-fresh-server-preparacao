c70b59b8832435303dd59f076d54141ab7f003e0
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-08 13:12:42 -0300

    chore(order): remove `orderId` from `ChangeItemOrderDto`

--- src/order/dto/change-item-order.dto.ts
+++ src/order/dto/change-item-order.dto.ts
@@ -1,12 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { IsArray, IsNotEmpty, IsUUID } from 'class-validator';
 
 export class ChangeItemOrderDto {
-  @IsString()
-  @IsNotEmpty()
-  @ApiProperty()
-  orderId: string;
-
   @IsArray()
   @IsNotEmpty()
   @IsUUID(undefined, {
