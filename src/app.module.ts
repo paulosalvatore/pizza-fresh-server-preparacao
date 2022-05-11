ca48cd888531864e0fda1763fb896c6a6966a1da
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-11 01:03:38 -0300

    fix: add `PassportModule` to all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TableModule } from './table/table.module';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { TableModule } from './table/table.module';
 import { UserModule } from './user/user.module';
-import { OrderModule } from './order/order.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TableModule, PrismaModule, ProductModule, UserModule, OrderModule, AuthModule],
+  imports: [
+    TableModule,
+    PrismaModule,
+    ProductModule,
+    UserModule,
+    OrderModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
